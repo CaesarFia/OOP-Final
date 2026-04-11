6aa62171b0be77f3fb3b401995c9972a8edb2c9f
lucas <146390150+lucasrodr1010@users.noreply.github.com>
2026-04-10 21:20:22 -0400

    complete checkpoint 2 scenarios

--- src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
+++ src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
@@ -1,33 +1,67 @@
 package oop.project.library.scenarios;
 
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.impl.Arguments;
-import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import oop.project.library.argument.ArgumentType;
+import oop.project.library.argument.Validators;
+import oop.project.library.command.Command;
+import oop.project.library.command.CommandException;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Map;
 
 public final class ArgumentScenarios {
 
+    private static final Command ADD = Command.builder("add")
+        .addParameter("left", ArgumentType.integer()).positional().add()
+        .addParameter("right", ArgumentType.integer()).positional().add()
+        .build();
+
+    private static final Command SUB = Command.builder("sub")
+        .addParameter("left", ArgumentType.dbl()).positional().add()
+        .addParameter("right", ArgumentType.dbl()).positional().add()
+        .build();
+
+    private static final Command FIZZBUZZ = Command.builder("fizzbuzz")
+        .addParameter("number", ArgumentType.integer().validate(Validators.range(1, 100))).positional().add()
+        .build();
+
+    private static final Command DIFFICULTY = Command.builder("difficulty")
+        .addParameter(
+            "difficulty",
+            ArgumentType.string().validate(Validators.choices(List.of("peaceful", "easy", "normal", "hard")))
+        ).positional().add()
+        .build();
+
+    private static final Command DATE = Command.builder("date")
+        .addParameter("date", ArgumentType.custom("LocalDate", LocalDate::parse)).positional().add()
+        .build();
+
     public static Map<String, Object> add(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        return parseScenario(ADD, arguments);
     }
 
     public static Map<String, Object> sub(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        return parseScenario(SUB, arguments);
     }
 
     public static Map<String, Object> fizzbuzz(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        return parseScenario(FIZZBUZZ, arguments);
     }
 
     public static Map<String, Object> difficulty(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        return parseScenario(DIFFICULTY, arguments);
     }
 
     public static Map<String, Object> date(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        return parseScenario(DATE, arguments);
+    }
+
+    private static Map<String, Object> parseScenario(Command command, String arguments) {
+        try {
+            return command.parse(arguments).toMap();
+        } catch (CommandException e) {
+            throw new RuntimeException("Invalid " + command.name() + " command.", e);
+        }
     }
 
 }
